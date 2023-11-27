f2074f87034277ab2c068d24f2f648ac37a43f00
KateGordon21 <kate.gordon@mnsu.edu>
2023-11-27 15:17:10 -0600

    home page changes

--- front-end/src/components/Home.tsx
+++ front-end/src/components/Home.tsx
@@ -1,9 +1,31 @@
 import React from "react";
-import '../styles/Home.css'
+import '../styles/Home.css';
+import me from '../assets/me.png';
+import { useState } from "react";
+
 
 const Home: React.FC = () => {
+    const [admire, setAdmire] = useState(true);
+
+    function showBackground() {
+        var element = document.getElementById("background") as HTMLDivElement;
+        var currentOpacity = window.getComputedStyle(element).getPropertyValue("opacity");
+        var newOpacity = currentOpacity === "1" ? "0.08" : "1";
+        element.style.opacity = newOpacity;
+        setAdmire(!admire);
+    }
+    
+
     return (
-        <main className="home">
+        <main>
+            <div id="background" className="background-image"></div>
+            {admire && <div id="img-description-container" className="img-description-container">
+                <div className="description-container"></div>
+                <img src={me}></img>
+            </div>}
+            <button onClick={showBackground}>Admire Background</button>
+            
+            
         </main>
     )
 }
